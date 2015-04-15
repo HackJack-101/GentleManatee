c05987f8767bcb568e7e756a621c711cbe570a84
Jack3113 <jack03113@gmail.com>
2015-04-15 04:45:42 +0200

    Update footer info

--- layouts/footer.php
+++ layouts/footer.php
@@ -2,6 +2,8 @@
 
 			<br/>
 			<footer style="text-align: center; font-style: italic">
+				Sources are hosted on Github : <a href="https://github.com/AamuLumi/GentleManateeAPI" target="_blank">API server</a> and <a href="https://github.com/Jack3113/GentleManatee" target="_blank">web server</a><br/>
+				Developed by <a href="https://twitter.com/AamuLumi" target="_blank">Aamu Lumi</a> and <a href="https://twitter.com/J_ck3113" target="_blank">Hackjack</a><br/>
 				This product is not endorsed, certified or otherwise approved in any way by Riot Games, Inc. or any of its affiliates.<br/>
 				© 2015 Riot Games, Inc. All rights reserved. Riot Games, League of Legends and PvP.net are trademarks, services marks, or registered trademarks of Riot Games, Inc.
 			</footer>
